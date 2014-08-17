66840186314c3cf80c0ee3b7adc362519476e6d7
Connor Williams <jricacosta+github@gmail.com>
2014-08-16 17:30:48 -0700

    improve frequency analysis, refactor

--- src/Crypto/Common.hs
+++ src/Crypto/Common.hs
@@ -10,22 +10,29 @@ where
 import           Data.Bits ((.|.), (.&.), shiftL, shiftR, xor)
 import qualified Data.ByteString.Base64.Lazy as B64 (encode)
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString.Lazy.Char8 as BL8 (pack, unpack)
 import           Data.Char (ord, chr)
+import           Control.Arrow (first, second)
+import           Control.Applicative ((<$>))
+import           Debug.Trace
 import qualified Data.Map.Strict as M
 import           Data.Word
+
+--for debugging only
+_p :: Show a => [Char] -> a -> a
+_p str a = trace (str ++ show a) a
 
 toB64 :: BL.ByteString -> BL.ByteString
 toB64 = B64.encode
 
 asciiToHex :: BL.ByteString -> BL.ByteString
 asciiToHex = BL.pack . asciiToHex' . BL.unpack
-  where asciiToHex' :: [Word8] -> [Word8]
-        asciiToHex' [] = []
-        asciiToHex' [x] = [shiftL x 4]
-        asciiToHex' (x:y:xs) = ((shiftL (conv x) 4) .|. (conv y)) : asciiToHex' xs
-        conv :: Word8 -> Word8
-        conv x = case x of
+
+asciiToHex' :: [Word8] -> [Word8]
+asciiToHex' [] = []
+asciiToHex' [x] = [shiftL x 4]
+asciiToHex' (x:y:xs) = ((shiftL (conv x) 4) .|. (conv y)) : asciiToHex' xs
+  where conv :: Word8 -> Word8
+        conv w = case w of
             0x30 -> 0x0
             0x31 -> 0x1
             0x32 -> 0x2
@@ -51,17 +58,18 @@ asciiToHex = BL.pack . asciiToHex' . BL.unpack
             0x65 -> 0xe
             0x66 -> 0xf
 
-            c -> error $ "invalid hex character: " ++ (show . chr . fromIntegral $ c)
+            _ -> error $ "invalid hex character: " ++ (show . chr . fromIntegral $ w)
 
 hexToAscii :: BL.ByteString -> BL.ByteString
 hexToAscii = BL.pack . hexToAscii' . BL.unpack
-  where hexToAscii' :: [Word8] -> [Word8]
-        hexToAscii' [] = []
-        hexToAscii' (x:xs) = let h1 = shiftR (x .&. 0xf0) 4
-                                 h2 = x .&. 0x0f
-                             in conv h1 : conv h2 : hexToAscii' xs
-        conv :: Word8 -> Word8
-        conv x = case x of
+
+hexToAscii' :: [Word8] -> [Word8]
+hexToAscii' [] = []
+hexToAscii' (x:xs) = let h1 = shiftR (x .&. 0xf0) 4
+                         h2 = x .&. 0x0f
+                     in conv h1 : conv h2 : hexToAscii' xs
+  where conv :: Word8 -> Word8
+        conv w = case w of
            0x0 -> 0x30
            0x1 -> 0x31
            0x2 -> 0x32
@@ -83,73 +91,89 @@ hexToAscii = BL.pack . hexToAscii' . BL.unpack
            _ -> error "impossible1"
 
 lbsXOR :: BL.ByteString -> BL.ByteString -> BL.ByteString
-lbsXOR bs1 bs2 = let ws1 = BL.unpack bs1
-                     ws2 = BL.unpack bs2
-                 in BL.pack $ zipWith xor ws1 ws2
+lbsXOR bs1 bs2 = BL.pack $ lbsXOR' bs1 bs2
+
+lbsXOR' :: BL.ByteString -> BL.ByteString -> [Word8]
+lbsXOR' bs1 bs2 = let ws1 = BL.unpack bs1
+                      ws2 = BL.unpack bs2
+                  in w8sXOR ws1 ws2
+
+
+w8sXOR :: [Word8] -> [Word8] -> [Word8]
+w8sXOR w8s1 w8s2 = zipWith xor w8s1 w8s2
 
 
 genCandidates :: BL.ByteString -> [((Char, BL.ByteString), Double)]
-genCandidates lbs = fmap distToEnglish . charStringPairs $ asciiToHex lbs
-  where charStringPairs s = fmap (xorAndConvertToString s) singleCharStrings
-        xorAndConvertToString lbs2 (c, lbs1) = (c, BL8.unpack $ lbsXOR lbs1 lbs2)
-        distToEnglish (c, l) = ((c, BL8.pack l), totalDistance l)
-        singleCharStrings = let f c = (chr c, BL8.pack . repeat . chr $ c)
-                            in fmap f [0..127]
+genCandidates lbs =
+    let hex = asciiToHex' . BL.unpack $ lbs
+    in rank . label <$> [(w, w8sXOR hex $ repeat w) | w <- [0..127]]
+  where rank = second totalDistance
+        label (w, l) = ((chr $ fromIntegral w, BL.pack l), l)
 
---TODO this algorithm shouldn't work as well as it does. Find another one.
-totalDistance :: [Char] -> Double
-totalDistance str = let len = fromIntegral $ length str
-                        counts = countChars str
-                        percentages = M.map (/ len) counts
-                    in sum . M.elems $ M.mapWithKey distFromCanonical percentages
-  where distFromCanonical k v = case M.lookup k frequenciesCanonical of
-                                    Nothing -> v
-                                    Just d -> abs (v - d)
 
-countChars :: [Char] -> M.Map Char Double
-countChars str = let charCounts = foldr incr M.empty (fmap toLower str)
-                 in  M.union charCounts frequenciesCanonical
+totalDistance :: [Word8] -> Double
+totalDistance str =
+    let len = fromIntegral $ length str
+        counts = countChars str
+        percentages =(/ len) . fromIntegral <$> counts
+        percentages' = M.union percentages frequenciesCanonical
+    in sum . M.elems $ M.mapWithKey deviation percentages'
+  where deviation k v = case M.lookup k frequenciesCanonical of
+                            Nothing -> v
+                            Just d -> abs (v - d)
+
+
+countChars :: [Word8] -> M.Map Word8 Int
+countChars str = foldr incr M.empty (toLower <$> str)
   where incr c m = let c' = collapseNonAlpha c
-                   in case M.lookup c m of
-                        Nothing -> M.insert c' 1 m
-                        Just i -> M.insert c' (i + 1) m
+                       --penalize gibberish (or at least improbable) bytes
+                       weight = if c' < 0x20 || c' > 0x7e then 5 else 1
+                   in case M.lookup c' m of
+                        Nothing -> M.insert c' weight m
+                        Just i -> M.insert c' (i + weight) m
+
 
 -- from http://en.wikipedia.org/wiki/Letter_frequency
-frequenciesCanonical :: M.Map Char Double
-frequenciesCanonical = M.fromList [ ('a', 0.08167)
-                                  , ('b', 0.01492)
-                                  , ('c', 0.02782)
-                                  , ('d', 0.04253)
-                                  , ('e', 0.13000)
-                                  , ('f', 0.02228)
-                                  , ('g', 0.02015)
-                                  , ('h', 0.06094)
-                                  , ('i', 0.06966)
-                                  , ('j', 0.00153)
-                                  , ('k', 0.00772)
-                                  , ('l', 0.04025)
-                                  , ('m', 0.02406)
-                                  , ('n', 0.06749)
-                                  , ('o', 0.07507)
-                                  , ('p', 0.01929)
-                                  , ('q', 0.00095)
-                                  , ('r', 0.05987)
-                                  , ('s', 0.06327)
-                                  , ('t', 0.09056)
-                                  , ('u', 0.02758)
-                                  , ('v', 0.00978)
-                                  , ('w', 0.02360)
-                                  , ('x', 0.00150)
-                                  , ('y', 0.01974)
-                                  , ('z', 0.00074)
-                                  , (' ', 0.14000)
-                                  -- catchall for digits, punctuation, etc.
-                                  , ('P', 0.07000)
-                                  ]
+frequenciesCanonical :: M.Map Word8 Double
+frequenciesCanonical = M.fromList $ fmap (first (fromIntegral . ord))
+    [ ('a', 0.08167)
+    , ('b', 0.01492)
+    , ('c', 0.02782)
+    , ('d', 0.04253)
+    , ('e', 0.13000)
+    , ('f', 0.02228)
+    , ('g', 0.02015)
+    , ('h', 0.06094)
+    , ('i', 0.06966)
+    , ('j', 0.00153)
+    , ('k', 0.00772)
+    , ('l', 0.04025)
+    , ('m', 0.02406)
+    , ('n', 0.06749)
+    , ('o', 0.07507)
+    , ('p', 0.01929)
+    , ('q', 0.00095)
+    , ('r', 0.05987)
+    , ('s', 0.06327)
+    , ('t', 0.09056)
+    , ('u', 0.02758)
+    , ('v', 0.00978)
+    , ('w', 0.02360)
+    , ('x', 0.00150)
+    , ('y', 0.01974)
+    , ('z', 0.00074)
+    , (' ', 0.14000)
+    -- catchall for digits, punctuation, etc.
+    , ('P', 0.07000)
+    ]
 
-toLower :: Char -> Char
-toLower c = if ord c < 0x5b && ord c > 0x40 then chr (ord c + 0x20) else c
+toLower :: Word8 -> Word8
+toLower c = if c > 0x40 && c < 0x5b then c + 0x20 else c
 
 -- convert printable, non-lowercase alphabetic characters to 'P'
-collapseNonAlpha :: Char -> Char
-collapseNonAlpha c = if ord c < 0x7b && ord c > 0x60 || ord c < 0x21 then c else 'P'
+collapseNonAlpha :: Word8 -> Word8
+collapseNonAlpha c = if (c > 0x20 && c < 0x41) ||
+                        (c > 0x5a && c < 0x61) ||
+                        (c > 0x7a && c < 0x7f)
+                    then 0x50
+                    else c
